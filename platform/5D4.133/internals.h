ce2816ba292150ddb6a74d71c0392bd3751b57bf
Lithiuman <lithiumxn@gmail.com>
2021-12-03 19:50:12 -0600

    for comparison

--- platform/5D4.133/internals.h
+++ platform/5D4.133/internals.h
@@ -5,7 +5,5 @@
 /** This camera has a DIGIC VI chip */
 #define CONFIG_DIGIC_VI
 
-#define CONFIG_DUAL_DIGIC
-
 /** Digic 6 does not have bitmap font in ROM, try to load it from card **/
 #define CONFIG_NO_BFNT
